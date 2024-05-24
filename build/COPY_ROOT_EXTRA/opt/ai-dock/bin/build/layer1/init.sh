920a3615370e4fa6f89ec4bb7b03832d00d36051
Abhinav Gupta <abhinavzspace@gmail.com>
2024-05-24 14:46:15 +0530

    add custom nodes

--- build/COPY_ROOT_EXTRA/opt/ai-dock/bin/build/layer1/init.sh
+++ build/COPY_ROOT_EXTRA/opt/ai-dock/bin/build/layer1/init.sh
@@ -19,6 +19,8 @@ NODES=(
     "https://github.com/BadCafeCode/masquerade-nodes-comfyui"
     "https://github.com/cubiq/ComfyUI_InstantID"
     "https://github.com/ltdrdata/ComfyUI-Inspire-Pack"
+    "https://github.com/liusida/ComfyUI-AutoCropFaces"
+    "https://github.com/kijai/ComfyUI-KJNodes"
 )
 
 CHECKPOINT_MODELS=(
